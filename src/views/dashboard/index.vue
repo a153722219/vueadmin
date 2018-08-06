4c7115a03c67539cdc9ae0780acd46f27ee716e8
a153722219 <437816510@qq.com>
2018-08-06 18:00:18 +0800

    333332323

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -1,12 +1,31 @@
 <template>
   <div class="dashboard-container">
-    <div class="dashboard-text">name:{{name}}</div>
-    <div class="dashboard-text">roles:<span v-for='role in roles' :key='role'>{{role}}</span></div>
+    <panel-group @handleSetLineChartData="handleSetLineChartData"></panel-group>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import PanelGroup from './component/PanelGroup'
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
 
 export default {
   name: 'dashboard',
@@ -15,6 +34,14 @@ export default {
       'name',
       'roles'
     ])
+  },
+  components:{
+    PanelGroup
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
+    }
   }
 }
 </script>
