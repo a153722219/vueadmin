42e33e5107fc99ea623604e33f8d6e2b9c75eba9
a153722219 <437816510@qq.com>
2018-08-07 17:45:28 +0800

    8.7commit

--- src/views/table/index.vue
+++ src/views/table/index.vue
@@ -6,38 +6,61 @@
           {{scope.$index}}
         </template>
       </el-table-column>
-      <el-table-column label="Title">
+      <el-table-column label="用户名" width="200" align="center">
         <template slot-scope="scope">
-          {{scope.row.title}}
+          {{scope.row.username}}
         </template>
       </el-table-column>
-      <el-table-column label="Author" width="110" align="center">
+      <el-table-column label="注册时间" width="200" align="center">
         <template slot-scope="scope">
-          <span>{{scope.row.author}}</span>
+          <i class="el-icon-time"></i>
+          <span>{{scope.row.regTime}}</span>
         </template>
       </el-table-column>
-      <el-table-column label="Pageviews" width="110" align="center">
+      <el-table-column label="是否实名" width="150" align="center">
         <template slot-scope="scope">
-          {{scope.row.pageviews}}
+          {{scope.row.isReal}}
         </template>
       </el-table-column>
-      <el-table-column class-name="status-col" label="Status" width="110" align="center">
+
+      <el-table-column align="center" prop="created_at" label="在线时长" width="200">
+        <template slot-scope="scope">
+          <span>{{scope.row.display_time}}</span>
+        </template>
+      </el-table-column>
+      <el-table-column align="center" prop="created_at" label="所属公司" width="230">
+        <template slot-scope="scope">
+          <span>{{scope.row.company}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column class-name="status-col" label="角色信息" width="150" align="center">
+        <template slot-scope="scope">
+          <span>{{scope.row.Identity}}</span>
+        </template>
+      </el-table-column>
+
+      <el-table-column class-name="status-col" label="帐号状态" width="150" align="center">
         <template slot-scope="scope">
           <el-tag :type="scope.row.status | statusFilter">{{scope.row.status}}</el-tag>
         </template>
       </el-table-column>
-      <el-table-column align="center" prop="created_at" label="Display_time" width="200">
+
+
+      <el-table-column class-name="status-col" label="操作" align="center">
         <template slot-scope="scope">
-          <i class="el-icon-time"></i>
-          <span>{{scope.row.display_time}}</span>
+          <el-button size="mini" type="danger" >删除</el-button>
+          <el-button size="mini" type="primary">冻结</el-button>
+          <el-button size="mini" type="success">修改</el-button>
         </template>
       </el-table-column>
+
     </el-table>
   </div>
 </template>
 
 <script>
-import { getList } from '@/api/table'
+//import { getList } from '@/api/table'
 
 export default {
   data() {
@@ -49,9 +72,8 @@ export default {
   filters: {
     statusFilter(status) {
       const statusMap = {
-        published: 'success',
-        draft: 'gray',
-        deleted: 'danger'
+        normal: 'success',
+        disable: 'danger'
       }
       return statusMap[status]
     }
@@ -61,11 +83,46 @@ export default {
   },
   methods: {
     fetchData() {
-      this.listLoading = true
-      getList(this.listQuery).then(response => {
-        this.list = response.data.items
+      this.listLoading = true;
+//      getList(this.listQuery).then(response => {
+//        this.list = response.data.items
+//        this.listLoading = false
+//      })
+
+      setTimeout( ()=> {
+        this.list=[
+          {
+            username:"17606637573",
+            regTime:"2015-1-15",
+            isReal:"否",
+            status:"normal",
+            display_time:"53小时",
+            company:"广州亿达科技有限公司",
+            Identity:"普通用户"
+          },
+          {
+            username:"17606637573",
+            regTime:"2015-1-15",
+            isReal:"否",
+            status:"normal",
+            display_time:"53小时",
+            company:"广州亿达科技有限公司",
+            Identity:"普通用户"
+          },
+
+          {
+            username:"17606637573",
+            regTime:"2015-1-15",
+            isReal:"否",
+            status:"disable",
+            display_time:"53小时",
+            company:"广州亿达科技有限公司",
+            Identity:"普通用户"
+          }
+
+        ];
         this.listLoading = false
-      })
+      },2000);
     }
   }
 }
